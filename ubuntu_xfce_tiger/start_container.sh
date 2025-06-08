fe60bf60e239d465a9cfd26bfa3c009dac229859
saul <saul@example.com>
2025-06-08 14:48:54 +0200

    running in detached mode

--- ubuntu_xfce_tiger/start_container.sh
+++ ubuntu_xfce_tiger/start_container.sh
@@ -2,4 +2,5 @@
 #          -p 5900:5900 \
 #          --name my_ubxfce ubxfce
           
-docker run -it -p 5900:5900 --name my_tiger_xfce tiger_xfce
+#docker run -it -p 5900:5900 --name my_tiger_xfce tiger_xfce
+docker run -d -p 5900:5900 --name my_tiger_xfce tiger_xfce
